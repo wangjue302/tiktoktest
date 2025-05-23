4002bc382271d00ff64357beced74de764f36ba5
wangjue302 <293461140@qq.com>
2025-05-23 17:44:36 +0800

    update

--- test.js
+++ test.js
@@ -114,9 +114,9 @@ function clickMessageButtonRecursively() {
         return false;
     }
 
-    // while (!ensureAvatarVisible(commentAvatar[AVATAR_CLICK_COUNT])) {
-    //     // 滑动直到元素进入可视区域
-    // }
+    while (!ensureAvatarVisible(commentAvatar[AVATAR_CLICK_COUNT])) {
+        // 滑动直到元素进入可视区域
+    }
 
     const avatarBounds = commentAvatar[AVATAR_CLICK_COUNT].bounds();
     if (!avatarBounds) {
