333842b575783a19a93fccb897f5fe160724a7da
wangjue302 <293461140@qq.com>
2025-05-26 17:11:20 +0800

    update

--- test.js
+++ test.js
@@ -103,7 +103,7 @@ function clickMessageButtonRecursively() {
 
     // const clickableAvatarParent = findClickableParent(commentAvatar[AVATAR_CLICK_COUNT]);
     const avatarBounds = commentAvatar[AVATAR_CLICK_COUNT].bounds();
-    toast("索引: " + AVATAR_CLICK_COUNT);
+    toast("当前索引: " + AVATAR_CLICK_COUNT);
     if (!avatarBounds) {
         toast("未获取到头像控件坐标");
         closeAndBack();
